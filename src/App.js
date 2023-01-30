8d8fddeb27c4526c1a588eb035b2d314a2eaadb9
Rkesh Roushan <rakeshroushan2341@gmail.com>
2023-01-31 00:07:47 +0530

    Working with Stats

--- src/App.js
+++ src/App.js
@@ -24,6 +24,11 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) => { 
+    console.log('In app.js');
+    console.log(expense);
+  }
 // return React.createElement(
 //     'div',
 //     {},
@@ -32,7 +37,7 @@ function App() {
 //   );
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
